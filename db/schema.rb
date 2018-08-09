85952a2e5bf300d92cd707d3ba281a841d5b878a
kckw0917 <kckw0917@gmail.com>
2018-08-09 10:06:16 +0000

    redoing

--- db/schema.rb
+++ db/schema.rb
@@ -10,58 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_085648) do
-
-  create_table "likes", force: :cascade do |t|
-    t.integer "item_id", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "purchase_datails", force: :cascade do |t|
-    t.integer "item_id", null: false
-    t.integer "price", null: false
-    t.integer "quantity", null: false
-    t.integer "purchase_id", null: false
-    t.datetime "purchase_date", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "purchases", force: :cascade do |t|
-    t.datetime "purchased_date", null: false
-    t.integer "total_price", null: false
-    t.integer "status", limit: 1, default: 0, null: false
-    t.string "delivery_address", default: "", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.text "comment", default: "", null: false
-    t.integer "item_id", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
