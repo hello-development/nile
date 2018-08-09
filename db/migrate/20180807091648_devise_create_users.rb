0be53f02433d3945dd978ca1bb59ba865454f2a0
RYOKOtakahashi <ryoko.1109@gmail.com>
2018-08-09 20:00:09 +0900

    created M and TB

--- db/migrate/20180807091648_devise_create_users.rb
+++ db/migrate/20180807091648_devise_create_users.rb
@@ -32,7 +32,18 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.integer :user_id
+      t.string :last_name
+      t.string :first_name
+      t.string :last_name_kana
+      t.string :first_name_kana
+      t.string :phone_number
+      t.string :email
+      t.string :encrypted_password
+      t.integer :artist_id
+      t.datetime :deleted_at
+      t.datetime :created_at
+      t.datetime :updated_at
       t.timestamps null: false
     end
 
