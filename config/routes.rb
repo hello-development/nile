5e4cfbed53b3d199cb22051269f7611f82b984d0
tada <tadahiro0402@gmail.com>
2018-08-11 15:48:40 +0900

    devise-admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-devise_for :admins, controllers: {
-  sessions:      'admins/sessions',
-  passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
-}
+
+devise_for :admins
 devise_for :users, controllers: {
   sessions:      'users/sessions',
   passwords:     'users/passwords',
