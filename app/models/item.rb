1455c598964c4dafd4fae51608c6f4660381ec3a
tada <tadahiro0402@gmail.com>
2018-08-12 13:32:00 +0900

    model編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+	belongs_to :artist
+	belongs_to :label
+	belongs_to :genre
+	has_many :disks, dependent: :destroy
+	has_many :purchase_datails
+	has_many :cart_items
+	has_many :likes, dependent: :destroy
+	has_many :reviews, dependent: :destroy
+
+	validates :artist :label :genre ,  presence: true
+	attachment :item_image
+
 end
--- app/models/purchase_datail.rb
+++ app/models/purchase_datail.rb
@@ -1,2 +1,7 @@
 class PurchaseDatail < ApplicationRecord
+	belongs_to :item
+	belongs_to :purchase
+
+	validates :purchase,  presence: true
+
 end
