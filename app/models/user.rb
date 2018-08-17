31a3707cc72f671f5af0c0a13517d07feba681e3
RYOKOtakahashi <ryoko.1109@gmail.com>
2018-08-17 18:35:53 +0900

    revised the view for registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,28 @@ class User < ApplicationRecord
    belongs_to :artist
    belongs_to :purchase
 
+    validates :last_name,
+    presence: true
+
+    validates :first_name,
+    presence: true
+
+    validates :last_name_kana,
+    presence: true
+
+    validates :first_name_kana,
+    presence: true
+
+    validates :phone_number,
+    presence: true
+
+    validates :email,
+    presence: true
+
+    validates :password,
+    presence: true,
+    uniqueness: true,
+    length: { minimum: 6 },
+    format: { with: /\A[a-z0-9]+\z/i }
+
 end
