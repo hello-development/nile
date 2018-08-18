0863b62255274b832da0b2691519f464cd72e0ed
kckw0917 <kckw0917@gmail.com>
2018-08-18 08:49:09 +0000

    Add address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,22 +1,44 @@
 class AddressesController < ApplicationController
 	def new
-      @user = User.find params[:user_id]
 	  @address = Address.new
 	end
 
 	def create
-	  @user = User.find params[:user_id]
 	  @address = Address.new(address_params)
-	  @address.user_id = @user.id
-	  address_name = @user.last_name + @user.first_name
-	  @address.address_name = address_name if @address.address_name.nil?
+	  @address.user_id = current_user.id
+	  @address.address_name = current_user.last_name + current_user.first_name if @address.address_name.nil?
 	  if @address.save
-	  	redirect_to user_path(@user)
+	  	redirect_to user_address_path(current_user, @address)
 	  else
 	  	render :new
 	  end
 	end
 
+    def show
+      @address = Address.new
+      @addresses = current_user.addresses
+    end
+
+    def edit
+      @address = Address.find(params[:id])
+    end
+
+    def update
+      @address = Address.find(params[:id])
+      if @address.update(address_params)
+      	redirect_to user_address_path(current_user, @address)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @user = current_user
+      @address = Address.find_by(user_id: params[:user_id], id: params[:id])
+      @address.destroy
+      @addresses = current_user.addresses
+      redirect_to user_address_path(current_user, @address)
+    end
 
 
    private
--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,5 @@ class Address < ApplicationRecord
 	belongs_to :user
 
 	validates :user, presence: true
+	validates :postal_code, length: { minimum: 7, maximum: 7 }
 end
