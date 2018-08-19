c3d989d9e8f24b5701c8af0d8522d9ee3434419e
kckw0917 <kckw0917@gmail.com>
2018-08-19 05:54:42 +0000

    Fix disks

--- app/models/disk.rb
+++ app/models/disk.rb
@@ -3,6 +3,10 @@ class Disk < ApplicationRecord
 	belongs_to :item
 	has_many :songs, dependent: :destroy
 
-	validates_presence_of :disk_number
-	validates_presence_of :item_id
+	validates :disk_number, presence: true
+	validates :disk_number, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+	validates :item_id, presence:true
+
+	default_scope -> { order(disk_number: :asc) }
+
 end
