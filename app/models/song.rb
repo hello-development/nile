b37f69d1fd0f31ab8e866082dbea02e9afa4c744
kckw0917 <kckw0917@gmail.com>
2018-08-19 05:43:17 +0000

    Fix songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,9 @@ class Song < ApplicationRecord
 	belongs_to :disk
 
 	validates :song_order,  presence: true
+	validates :song_order, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 	validates :song_name,  presence: true
+
+    default_scope -> { order(song_order: :asc) }
 
 end
