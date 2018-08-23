6e54acf4c051424a11de683c1e321b2e89543bb6
kckw <39869860+kckw0917@users.noreply.github.com>
2018-08-23 21:36:09 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,7 +48,6 @@ class ItemsController < ApplicationController
 		    end
 		  end
 		return
-		render :index, layout: "item"# itemのレイアウトを適用させる。
 		end
 		render :index, layout: "item"
 		# itemのレイアウトを適用させる。
